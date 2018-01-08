99f72fb9af3de4c166bf57796e0f6f770d18de08
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-01-08 16:21:40 +0530

    Added css property for animation

--- ch05/begin/scripts/my_scripts.js
+++ ch05/begin/scripts/my_scripts.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-    animateMe();
-    function animateMe() {
-        $(".my-div").animate({top: "500px"}, 1500).animate({top: "-500px"}, 1500);
-        animateMe();
-    }
 
 });
 
